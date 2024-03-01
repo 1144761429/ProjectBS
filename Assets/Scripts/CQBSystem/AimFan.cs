4985bd37d17648047bb379f8ca669430085156e9
0doxes0 <0doxes0@gmail.com>
2024-03-01 08:13:52 -0700

    Two Aim-fans ready
    The Aim Fan is now working and at the ready, there is a liner version and a planer version. They may not be used  towards the end, but the codes can still be used for other purposes.

--- Assets/Scripts/CQBSystem/AimFan.cs
+++ Assets/Scripts/CQBSystem/AimFan.cs
@@ -6,8 +6,8 @@ public class AimFan : MonoBehaviour
 {
     public Transform player; // The starting point of the raycast
     public float maxRange = 100f; // Maximum range of the weapon
-    public float spreadAngle = 10f; // The spread angle of the weapon
-    public int rayCount = 32; // How many rays to cast within the spread cone
+    public float spreadAngle = 120f; // The spread angle of the weapon
+    public int rayCount = 128; // How many rays to cast within the spread cone
     public LayerMask hitLayers; // Layers that the ray should collide with
     private LineRenderer lineRenderer;
 
@@ -28,7 +28,7 @@ public class AimFan : MonoBehaviour
         Vector3[] points = new Vector3[2*rayCount + 2];
         points[0] = player.position; // The first point is the origin
 
-        float stepAngle = spreadAngle / (float)(rayCount - 1);
+        float stepAngle = 2*spreadAngle / (float)(rayCount - 1);
         for (int i = 0; i < rayCount; i++)
         {
             float angle = -spreadAngle * 0.5f + stepAngle * i; // Rotate around the forward axis
