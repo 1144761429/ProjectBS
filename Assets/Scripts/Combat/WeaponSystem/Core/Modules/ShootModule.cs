e67af1e5ba3b35531192231aba1767de07f2216c
1144761429 <1144761429bzy@gmail.com>
2024-03-07 18:34:10 -0700

    Updated ShootModule

--- Assets/Scripts/Combat/WeaponSystem/Core/Modules/ShootModule.cs
+++ Assets/Scripts/Combat/WeaponSystem/Core/Modules/ShootModule.cs
@@ -18,27 +18,61 @@ namespace Combat.WeaponSystem.Core.Modules
     public sealed class ShootModule : WeaponModule
     {
         #region Events
+        
+        /// <summary>
+        /// An event that will trigger after passing all the <see cref="_shootCondition"/>, and before perform a shoot.
+        /// </summary>
         public EventHandler<WeaponShootEventArgs> BeforeShoot { get; set; }
+        
+        /// <summary>
+        /// An event that will trigger when is shooting(right after the <see cref="Projectile"/> is set active and shoot
+        /// out).
+        /// </summary>
         public EventHandler<WeaponShootEventArgs> OnShoot { get; set; }
+        
+        /// <summary>
+        /// An event that will trigger after <see cref="OnShoot"/>.
+        /// </summary>
         public EventHandler<WeaponShootEventArgs> AfterShoot { get; set; }
         
         #endregion
 
         #region Fields and Properties
-
+        
+        /// <inheritdoc/>
         public override EWeaponModuleType ModuleType => EWeaponModuleType.ShootModule;
 
+        /// <summary>
+        /// The <see cref="ShootPattern"/> that this <see cref="ShootModule"/> follows. This is provided by the
+        /// <see cref="WeaponModule.SourceWeapon"/>.
+        /// </summary>
         private ShootPattern _shootPattern;
 
+        /// <summary>
+        /// The <see cref="WeaponModule.SourceWeapon"/> casted to <see cref="RangeWeapon"/>.
+        /// This will be null if the cast fail. If a cast fails, an <see cref="IncompatibleModuleException"/> will
+        /// be thrown.
+        /// </summary>
         private RangeWeapon _sourceRangeWeapon;
 
+        /// <summary>
+        /// The condition for if this <see cref="ShootModule"/> can shoot.
+        /// </summary>
         private Func<bool> _shootCondition;
-        
+
         #endregion
         
         #region Public Function
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Create a <see cref="ShootModule"/> that is attached to <see cref="WeaponModule.SourceWeapon"/>.
+        /// </summary>
+        /// 
+        /// <param name="sourceWeapon">The <see cref="Weapon"/> that this <see cref="WeaponModule"/> is attached on.</param>
+        /// <param name="shootPattern">The <see cref="ShootPattern"/> that this <see cref="ShootModule"/> follows.</param>
+        /// 
+        /// <exception cref="IncompatibleModuleException">If the <paramref name="sourceWeapon"/> is not compatible with
+        /// this module.</exception>
         public ShootModule(Weapon sourceWeapon, ShootPattern shootPattern) : base(sourceWeapon)
         {
             if (SourceWeapon is not RangeWeapon)
@@ -49,16 +83,17 @@ namespace Combat.WeaponSystem.Core.Modules
 
             _sourceRangeWeapon = (RangeWeapon)SourceWeapon;
             _shootPattern = shootPattern;
+            
+            //TODO: change the shoot condition according to the fire mode of the weapon.
         }
         
         /// <summary>
-        /// Shoot the <see cref="_sourceRangeWeapon"/>and trigger events accordingly.
+        /// Shoot the <see cref="_sourceRangeWeapon"/>.
         /// </summary>
         /// 
         /// <param name="direction">The direction to shoot.</param>
-        /// 
-        /// <returns><c>true</c> if shoot successfully. Otherwise, meaning the <see cref="_shootCondition"/> is not
-        /// satisfied, return <c>false</c>.</returns>
+        ///
+        /// <returns><c>true</c> if shoot is successfully performed. Otherwise, <c>false</c>.</returns>
         public bool Shoot(Vector3 direction)
         {
             if (!_shootCondition.AllTrue())
@@ -66,32 +101,30 @@ namespace Combat.WeaponSystem.Core.Modules
                 return false;
             }
             
-            WeaponShootEventArgs eventArgs =
-                new WeaponShootEventArgs(_sourceRangeWeapon.DatabaseID, _sourceRangeWeapon.Wielder);
-            
-            BeforeShoot?.Invoke(this, eventArgs);
             _sourceRangeWeapon.StartCoroutine(ProcessShootPattern(direction));
-            
-            // TODO: Make the function continue the execution until the coroutine above is finished
-            OnShoot?.Invoke(this, eventArgs);
-            AfterShoot.Invoke(this,eventArgs);
-            
+
             return true;
         }
-
+        
         #endregion
         
         #region Private Functions
-
+        
         /// <summary>
         /// Shoot at direction <paramref name="direction"/> and apply direction modification described in
         /// the <see cref="ShootPattern"/> of the <see cref="Weapon"/> this <see cref="ShootModule"/> attached to.
+        /// And trigger events of <see cref="BeforeShoot"/>, <see cref="OnShoot"/>, and <see cref="AfterShoot"/> accordingly.
         /// </summary>
         /// 
         /// <param name="direction">The direction to shoot. This is a general direction. The actual shoot direction will
         /// be affected by the direction and offset described in <see cref="ShootPattern"/>. </param>
         private IEnumerator ProcessShootPattern(Vector3 direction)
         {
+            WeaponShootEventArgs eventArgs =
+                new WeaponShootEventArgs(_sourceRangeWeapon.DatabaseID, _sourceRangeWeapon.Wielder);
+            
+            BeforeShoot?.Invoke(this, eventArgs);
+            
             // Retrieve all the projectile prefab that is needed.
             _sourceRangeWeapon.AmmunitionModule.GetProjectile(_shootPattern.ProjectileCount, out Projectile[] projectiles);
             
@@ -128,6 +161,9 @@ namespace Combat.WeaponSystem.Core.Modules
 
                 yield return new WaitForSeconds(round.IntervalBeforeNext);
             }
+            
+            OnShoot?.Invoke(this, eventArgs);
+            AfterShoot.Invoke(this,eventArgs);
         }
 
         #endregion
